66d7d81847633368c8138ddf81e5547c42bd6146
Jeremiah <Lovettjeremiah@gmail.com>
2025-05-19 21:34:05 -0500

    Initial Commit

---
+++ Pure/PureApp.swift
@@ -0,0 +1,17 @@
+//
+//  PureApp.swift
+//  Pure
+//
+//  Created by kk on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct PureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PureTests/PureTests.swift
@@ -0,0 +1,16 @@
+//
+//  PureTests.swift
+//  PureTests
+//
+//  Created by kk on 5/19/25.
+//
+
+import Testing
+
+struct PureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
